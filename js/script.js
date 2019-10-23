3bccfec5bed5ccac755775bffa9304099f2410d7
Małgorzata Brożek <gosikbrozek1997@gmail.com>
2019-10-23 21:38:02 +0200

    Add class 'active' to the correct article

--- js/script.js
+++ js/script.js
@@ -1,29 +1,31 @@
 'use strict';
 
 function titleClickHandler(event) {
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
 
-    /* [DONE] remove class 'active' from all article links  */
+    /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
     }
-    /* [IN PROGRESS] add class 'active' to the clicked link */
-    console.log('clickedElement:', clickedElement);
+    /* add class 'active' to the clicked link */
     clickedElement.classList.add('active')
-    /* [DONE] remove class 'active' from all articles */
+    /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
 
     for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
     }
     /* get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
 
     /* find the correct article using the selector (value of 'href' attribute) */
-
+    const targetArticle = document.querySelector(articleSelector);
     /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');

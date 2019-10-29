60e72b4a8cff2e4a53b23ebeb169abde62db4489
Małgorzata Brożek <gosikbrozek1997@gmail.com>
2019-10-29 19:49:40 +0100

    Add tagCliclHandler

--- js/script.js
+++ js/script.js
@@ -85,7 +85,7 @@ function generateTags(){
     /* START LOOP: for each tag */
     for(const tag of tags) {
       /* generate HTML of the link */
-      const tagHTML = "<li><a>" + tag + "</a></li>";
+      const tagHTML = "<li><a href=\"#tag-" + tag + "\">" + tag + "</a></li>";
       /* add generated code to html variable */
       html += tagHTML;
     /* END LOOP: for each tag */
@@ -96,3 +96,48 @@ function generateTags(){
 }
 }
 generateTags();
+
+function tagClickHandler(event){
+  /* prevent default action for this event */
+  event.preventDefault();
+  /* make new constant named "clickedElement" and give it the value of "this" */
+  const clickedElement = this;
+  /* make a new constant "href" and read the attribute "href" of the clicked element */
+  const href = clickedElement.getAttribute('href');
+  /* make a new constant "tag" and extract tag from the "href" constant */
+  const tag = href.replace('#tag-', '');
+  /* find all tag links with class active */
+  const activeTags = document.querySelectorAll('a.active[href^="#tag-"]') 
+  /* START LOOP: for each active tag link */
+  for(const activeTag of activeTags) {
+    /* remove class active */
+    activeTag.classList.remove('active');
+  }
+  /* END LOOP: for each active tag link */
+
+  /* find all tag links with "href" attribute equal to the "href" constant */
+  const sameTagLinks = document.querySelectorAll('a[href="' + href +'"]'); 
+  
+  /* START LOOP: for each found tag link */
+  for(const sameTagLink of sameTagLinks){
+    /* add class active */
+    sameTagLink.classList.add('active');
+  }
+  /* END LOOP: for each found tag link */
+
+  /* execute function "generateTitleLinks" with article selector as argument */
+  generateTitleLinks('[data-tags~="' + tag + '"]');
+}
+
+function addClickListenersToTags(){
+  /* find all links to tags */
+  const links = document.querySelectorAll('.list-horizontal a');
+  /* START LOOP: for each link */
+  for(const link of links) {
+    /* add tagClickHandler as event listener for that link */
+    link.addEventListener('click', tagClickHandler);
+    
+  }
+  /* END LOOP: for each link */
+}
+addClickListenersToTags();
